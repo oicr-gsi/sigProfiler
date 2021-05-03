d67976200c4d58dc7beab8ad694fade1cd4f4b5b
alexjfortuna <alexjfortuna@gmail.com>
2021-05-02 20:55:43 -0400

    add tests and calculate, fix sigpro python code

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 cd $1
 
-find . -regex '.*\.fusions.tsv$' -exec wc -l {} \;
-find . -regex '.*\.fusions.discarded.tsv$' -exec wc -l {} \;
-ls | sed 's/.*\.//' | sort | uniq -c
+find . -name decompositionprofile.csv -exec wc -l {} \;
+find . -name Mutation_Probabilities.txt -exec wc -l {} \;
+find . -name Sig_activities.txt -exec wc -l {} \;
+find . -name Signatures.txt -exec wc -l {} \;
